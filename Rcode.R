8f34a06cc80f5137f50c048dd8922e897b633d1f
Omar Benites Alfaro <omar_cs_7@hotmail.com>
2017-08-22 14:10:50 -0500

    traiittools.1.01

---
+++ Rcode.R
@@ -0,0 +1,3 @@
+drat::insertPackage(file = "sbformula_2.0.1.tar.gz",repodir = "D:/HIDAP_PACAKGES/GITHUB/drat/")
+
+drat::insertPackage(file = "D:/HIDAP_PACAKGES/GITHUB/traittools_1.0.1.tar.gz",repodir = "D:/HIDAP_PACAKGES/GITHUB/drat/")
